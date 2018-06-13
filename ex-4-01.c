4ee53457fa822d05faded3b556a6508df7f1ef47
Matt Pritchard <t3hsauce@skynet>
2018-06-13 17:51:35 -0400

    fixes mistake in the input string

--- ex-4-01.c
+++ ex-4-01.c
@@ -19,11 +19,10 @@ int main()
 {
     char string[100] = "Jet fuel can\'t melt steel beams";
     char character = 'b';
-    printf("The length of the input string is %lu.\n", strlen(string));
     if (strrindex(string, character) == -1) {
         printf("Character %c does not appear in the string.\n", character);
     } else {
-        printf("The leftmost occurence of character %c appears at index %d.\n",
+        printf("The rightmost occurence of character %c appears at index %d.\n",
                character, strrindex(string, character));
     }
 }
